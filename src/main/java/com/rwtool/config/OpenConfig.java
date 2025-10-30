802b3a9ee78949ac565ef0d9b176f360c3b6baab
Mohan <mohanbmk007@gmail.com>
2025-10-30 16:24:46 +0530

    old src deleted

--- src/main/java/com/rwtool/config/OpenConfig.java
+++
@@ -1,8 +0,0 @@
-package com.rwtool.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenConfig {
-    // No OpenAPI/Swagger configuration as per stack restrictions
-}
