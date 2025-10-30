74191e3e7b939e4200912ca2bbcaace5a95bb85d
MohanBMK <142028779+MohanBMK@users.noreply.github.com>
2025-10-30 12:32:09 +0530

    Add files via upload
    cs

---
+++ RwtoolApplication.java
@@ -0,0 +1,12 @@
+package com.rwtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwtoolApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(RwtoolApplication.class, args);
+    }
+}
---
+++ SubscriptionStatusStartApp.java
@@ -0,0 +1,11 @@
+package subscriptionStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionStatusStartApp {
+    public static void main(String[]args){
+        SpringApplication.run(SubscriptionStatusStartApp.class,args);
+    }
+}
