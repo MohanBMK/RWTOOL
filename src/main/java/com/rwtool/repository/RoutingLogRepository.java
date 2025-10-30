1c1b603457fc9a174c9b8aa9b699f90c71c7979c
Mohan <mohanbmk007@gmail.com>
2025-10-30 17:56:12 +0530

    src deleted

--- src/main/java/com/rwtool/repository/RoutingLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rwtool.repository;
-
-
-import com.rwtool.model.RoutingLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutingLogRepository extends JpaRepository<RoutingLog, Long> {
-}
